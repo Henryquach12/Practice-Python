19037b8fc71734ec0eab35794d851219bd416897
Henry <haoquach1209@gmail.com>
2025-07-16 12:01:05 +0930

    First commit for Practice Python project

---
+++ Practice_Python/Python Number Guessing Game.py
@@ -0,0 +1,9 @@
+lowest_number = 1
+highest_number = 100
+answer = random.randint(lowest_number, highest_number)
+guesses = 0
+is_running = True
+
+print("Python Number Guessing Game!")
+while is_running:
+    print(f"Please guess a number between {}")
